47fd191dfce0637ebd2a7287e077f183be00c5bc
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-15 13:19:31 -0500

    Iteration 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Kode with Klossy. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
+
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Kode with Klossy. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    func createToDos() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+
+        
+        return [swift, dog]
+    }
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+            
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+                
+                
+        let toDo = toDos[indexPath.row]
+                
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+                
+        return cell
+           
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         //Get the new view controller using segue.destination.
+         //Pass the selected object to the new view controller.
+    }
+}
+
+
+
+
